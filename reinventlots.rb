709e825cb60147382a95ff6bdaaa85433fbcde8f
andrewxhill <andrewxhill@gmail.com>
2012-06-30 17:43:29 -0400

    api/add added new add method

--- reinventlots.rb
+++ reinventlots.rb
@@ -57,3 +57,21 @@ get '/api/create' do
   
   map.to_json
 end
+
+get '/api/add' do
+  map = {}
+  params.each_pair do |k,v|
+    map[k] = quote_string(v)
+  end
+  
+  sql = "INSERT INTO #{CARTODB_CONF['locations_table']}(the_geom, hash, name, description, address, imgur_orig, imgur_small, imgur_thumb) VALUES (ST_SetSRID(ST_MakePoint(#{map[:lng]},#{map[:lat]}),4326), '#{map[:hash]}', '#{map[:name]}', '#{map[:desc]}', '#{map[:address]}', '#{map[:orig]}', '#{map[:small]}', '#{map[:thumb]}')"
+  req = "http://#{CARTODB_CONF['host']}.cartodb.com/api/v2/sql"
+  
+  
+  params = {'q'=>sql, 'api_key'=>CARTODB_CONF['api_key']}
+  x = Net::HTTP.post_form(URI.parse(CARTODB_CONF['post_url']), params)
+  
+  map[:added] = time = Time.now.getutc
+  
+  map.to_json
+end
