7d8da4bf25b2bc2c4421ebbbdd6a5b28cbf73df2
Olex <tholex@gmail.com>
2012-06-30 12:20:38 -0400

    Initial commit with Gemfile, rvmrc

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+
+gem 'rack'
+gem 'shotgun'
+gem 'sinatra', "1.3.2"
---
+++ reinventlots.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'sinatra'
+
+
+get '/' do
+  "Reinvent Lots :: Placeholder"
+end
+
