e37237f40a227c135501d56e0147f2f33462c81d
andrewxhill <andrewxhill@gmail.com>
2012-06-30 17:30:53 -0400

    working /api/create

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ gem 'sinatra', "1.3.2"
 gem 'sinatra-twitter-oauth'
 gem 'sass'
 gem 'compass'
+gem 'json'
--- reinventlots.rb
+++ reinventlots.rb
@@ -2,6 +2,9 @@ require 'rubygems'
 require 'sinatra'
 require 'sass'
 require 'compass'
+require "json"
+require "uri"
+require "net/http"
 
 configure do
   Compass.configuration do |config|
@@ -27,4 +30,30 @@ end
 get '/css/main.css' do
   content_type 'text/css', :charset => 'utf-8'
   scss :main
+end
+
+
+#CARTODB_CONF = YAML.load_file(Rails.root.join('config/cartodb_config.yml'))[RAILS_ENV]
+CARTODB_CONF = YAML::load(File.read('config/cartodb_config.yml'))
+def quote_string astr
+  return astr.gsub(/\A['"]+|['"]+\Z/, "").gsub(/\\/, '\&\&').gsub(/'/, "''").gsub(';',' ')
+end
+
+get '/api/create' do
+  map = {}
+  params.each_pair do |k,v|
+    map[k] = quote_string(v)
+  end
+  map[:hash] = 'Fd35ki'
+  
+  sql = "INSERT INTO #{CARTODB_CONF['locations_table']}(the_geom, hash, name, description, address, imgur_orig, imgur_small, imgur_thumb) VALUES (ST_SetSRID(ST_MakePoint(#{map[:lng]},#{map[:lat]}),4326), '#{map[:hash]}', '#{map[:name]}', '#{map[:desc]}', '#{map[:address]}', '#{map[:orig]}', '#{map[:small]}', '#{map[:thumb]}')"
+  req = "http://#{CARTODB_CONF['host']}.cartodb.com/api/v2/sql"
+  
+  
+  params = {'q'=>sql, 'api_key'=>CARTODB_CONF['api_key']}
+  x = Net::HTTP.post_form(URI.parse(CARTODB_CONF['post_url']), params)
+  
+  map[:created] = time = Time.now.getutc
+  
+  map.to_json
 end
